8611584e7559cdc93e5687e301b6cec2743482c3
life-paradox <100072911+life-paradox@users.noreply.github.com>
2022-10-08 12:03:47 +0000

    Deploying to gh-pages from @ life-paradox/filmoteka_project8@908b51b9c02a4e06d5b499bbc8c46b5318213863 🚀

---
+++ index.79b94260.js
@@ -0,0 +1,2 @@
+(async()=>{const e=await fetch("https://api.themoviedb.org/3/trending/movie/week?api_key=861782ee1fc6aacf939bc06e51306075"),a=await e.json();console.log(a)})();
+//# sourceMappingURL=index.79b94260.js.map
