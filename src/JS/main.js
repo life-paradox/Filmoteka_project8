e6d210d457fa71ec290d33359f4353b20e60460d
Vladyslav Basalyha <100045506+VladBasalyha@users.noreply.github.com>
2022-10-10 01:39:00 +0300

    new commit

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,64 +1,70 @@
-import { API_KEY } from "./api-key";
-const galleryRef = document.querySelector(".gallery");
+import { API_KEY } from './api-key';
+const galleryRef = document.querySelector('.gallery');
 
 // фетч жанров
 
 // https://api.themoviedb.org/3/genre/movie/list?api_key=861782ee1fc6aacf939bc06e51306075&language=uk-UA
 function genres() {
-
-  if (localStorage.getItem("genres")) {
-  return
-  }
-  else {
-  const fetchGenres = async () => {
-  const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=uk-UA`);
-  const genres = await response.json();
-  console.log(genres);
-  localStorage.setItem("genres", JSON.stringify(genres));
+  if (localStorage.getItem('genres')) {
+    return;
+  } else {
+    const fetchGenres = async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=uk-UA`
+      );
+      const genres = await response.json();
+      console.log(genres);
+      localStorage.setItem('genres', JSON.stringify(genres));
     };
     fetchGenres();
-}
+  }
 }
 export { genres };
 
-
-
 // Фетч популярных фильмов
 const fetchPopFilms = async () => {
-  const response = await fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}&language=uk-UA&page=1`);
+  const response = await fetch(
+    `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}&language=uk-UA&page=1`
+  );
   const films = await response.json();
   console.log(films);
   return films;
 };
 export { fetchPopFilms };
 
-  
-  // фетч по ключевому слову
-  const fetchQueryFilm = async (query) => {
-  const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=uk-UA&query=${query}&page=1&include_adult=false`);
+// фетч по ключевому слову
+const fetchQueryFilm = async query => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=uk-UA&query=${query}&page=1&include_adult=false`
+  );
   const films = await response.json();
   console.log(films);
   return films;
 };
 export { fetchQueryFilm };
-  
-  // Рендер карточек
+
+// Рендер карточек
 function renderFilms(films) {
+  const markup = films.results
+    .map(
+      ({
+        title,
+        poster_path,
+        genre_ids,
+        release_date,
+        first_air_date,
+        year = release_date || first_air_date || ' - ',
+      }) => {
+        const genreName = genre_ids
+          .map(element =>
+            parseGenres.genres.find(genre => genre.id === element)
+          )
+          .map(element => element.name)
+          .join(', ');
+        //console.log(genreName);
 
-  const markup = films.results.map(({title, poster_path, genre_ids, release_date, first_air_date,
-    year = release_date || first_air_date || ' - ',
-  }) => { 
-
-    const genreName = genre_ids.map(element => 
-      parseGenres.genres.find(genre => genre.id === element)
-      ).map(element=> element.name).join(', ');
-      //console.log(genreName);
-      
-
-          return `<li class="gallery__item">
-            <a class="gallery__link" href="">
+        return `<li class="gallery__item">
                 <img class="gallery__image" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="" loading="lazy">
-            </a>
             <div class="gallery__info">
                 <p class="gallery__title">${title}</p>
 
@@ -66,8 +72,10 @@ function renderFilms(films) {
                 <p class="gallery__year">${year.slice(0, 4)}</p>
 
             </div>
-        </li>`
-      }).join('');
+        </li>`;
+      }
+    )
+    .join('');
   console.log(films.results);
   galleryRef.innerHTML = markup;
   return films;
@@ -75,6 +83,6 @@ function renderFilms(films) {
 export { renderFilms };
 
 //Парсінг жанрів
-const savedGenres = localStorage.getItem("genres");
+const savedGenres = localStorage.getItem('genres');
 const parseGenres = JSON.parse(savedGenres);
 console.log(parseGenres.genres);
---
+++ src/JS/modal-film.js
@@ -0,0 +1,7 @@
+function a() {
+  const mk = `<div class = 'backdrop'>
+  <div class = 'modal'>
+  </div>
+</div>`;
+  document.body.insertAdjacentHTML('afterbegin', mk);
+}
