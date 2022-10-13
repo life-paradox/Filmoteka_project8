02daf78bd332dac09eba2bd008dc5535f0f037da
DanielOsadchenko <dm.osadchenko@gmail.com>
2022-10-13 18:11:48 +0200

    searchQuery is ready to work)))

--- src/JS/pagination.js
+++ src/JS/pagination.js
@@ -1,7 +1,7 @@
 import { markupMovies } from './index-markups';
 
 import { paginationMarkup, paginationMarkupMobile } from './pagination-markups';
-import { fetchPopFilms, renderFilms } from './main';
+import { fetchPopFilms, renderFilms, fetchQueryFilm } from './main';
 
 export { pagination };
 export { currentPage };
@@ -131,3 +131,34 @@ function handlePageButtonsStatus() {
     enableButton(nextButton);
   }
 }
+
+
+function paginationforQuery(films) {
+  movies = films;
+
+  if (movies.results.length === 0) {
+    paginationNav.classList.add('hidden');
+    return;
+  }
+
+  // paginationNav.classList.remove('hidden');
+  pageCount = Math.ceil(movies.total_results / paginationLimit);
+  renderPage(films);
+
+  prevButton.addEventListener('click', () => {
+    fetchQueryFilm(currentPage - 1).then(renderPage);
+  });
+
+  nextButton.addEventListener('click', () => {
+    fetchQueryFilm(currentPage + 1).then(renderPage);
+  });
+
+  paginationWrapper.addEventListener('click', e => {
+    if (e.target.hasAttribute('page-index')) {
+      fetchQueryFilm(Number(e.target.getAttribute('page-index'))).then(
+        renderPage
+      );
+    }
+  });
+}
+export { paginationforQuery };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { fetchPopFilms, renderFilms } from './JS/main';
 import { renderFilms } from './JS/main';
 import { slider } from './JS/slider';
-import { pagination } from './JS/pagination';
+import { pagination, paginationforQuery } from './JS/pagination';
 import { fetchQueryFilm } from './JS/main';
 import { genres } from './JS/main';
 
@@ -11,15 +11,16 @@ fetchPopFilms(1).then(pagination);
 
 
 // поиск по ключевому слову
-
+let searchData;
+export { searchData };
 const formRef = document.querySelector('.header-search-form');
 formRef.addEventListener('submit', onSearch);
 function onSearch(e) {
   e.preventDefault();
-  const searchData = formRef.elements.searchQuery.value.trim();
+  searchData = formRef.elements.searchQuery.value.trim();
   console.log(searchData);
   if (searchData !== '') {
-    fetchQueryFilm(searchData).then(renderFilms);
+    fetchQueryFilm(1,searchData).then(paginationforQuery);
   }
 }
 
