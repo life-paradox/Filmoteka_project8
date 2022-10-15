591d1b983fab85848e14ad0d93e8f66e84fff547
Dmitriy-Hrihor <dmytro.hryhor@gmail.com>
2022-10-16 01:22:08 +0300

    fix to button

--- src/JS/themeRender.js
+++ src/JS/themeRender.js
@@ -1,12 +1,26 @@
-const themeRender = document.querySelector('#theme-day');
 const themeRenderFooter = document.querySelector('.footer');
-const titleRenderTheme = document.querySelector('.main');
+const titleRenderTheme = document.querySelector('body');
 const bgButtonToogl = document.querySelector('.theme-day');
 
-themeRender.addEventListener('click', onChengeTheme);
-function onChengeTheme() {
-  themeRenderFooter.classList.toggle('darkTheme');
-  titleRenderTheme.classList.toggle('darkTheme');
-  // titleRenderText.classList.toggle('darkTheme');
-  bgButtonToogl.classList.toggle('dark');
+const checkBox = document.getElementById('myCheck');
+
+checkBox.addEventListener('change', evt => {
+  evt.preventDefault();
+  if (checkBox.checked) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.removeItem('theme');
+  }
+  addDarkTheme();
+});
+
+function addDarkTheme() {
+  try {
+    if (localStorage.getItem('theme') === 'dark') {
+      titleRenderTheme.classList.add('darkTheme');
+    } else {
+      titleRenderTheme.classList.remove('darkTheme');
+    }
+  } catch (err) {}
 }
+addDarkTheme();
